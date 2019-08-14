eb8877361ee9364f1f51b515411bf125a47f6bc1
zhangx <zx_953@126.com>
2019-08-14 16:51:45 +0800

    添加菜单

--- ssq.py
+++ ssq.py
@@ -19,9 +19,9 @@ def random_red():
 
 
 def random_blue():
-    blue = list()
+    blue = []
     blue.append(random.choice(range(1, 13, 1)))
-    return blue
+    return blue 
 
 
 def select_1():
@@ -41,10 +41,31 @@ def select_n():
 def ssq():
     while True:
         select_n()
-        YN = raw_input("是否继续投注（Y/N)？")
-        if YN == 'N' or YN == 'n':
+        yn = raw_input("是否继续投注（Y/N)？")
+        if yn == 'N' or yn == 'n':
             break
 
 
+def input_num():
+    print("输入号码！！")
+
+
+def count_num():
+    print("统计号码！！")
+
+
 if __name__ == '__main__':
-    ssq()
+    while True:
+        print("1) 随机选择")
+        print("2) 号码录入")
+        print("3) 号码统计")
+        yn = input("请输入编号：")
+        if yn == 1:
+            ssq()
+        elif yn == 2:
+            input_num()
+        elif yn == 3:
+            count_num()
+        else:
+            break
+
